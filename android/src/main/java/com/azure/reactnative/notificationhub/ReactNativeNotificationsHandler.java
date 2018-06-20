c36a5883a4718616509c3454ba2856463e1d3919
Miguel Ángel <zlash@users.noreply.github.com>
2018-06-20 16:21:04 -0700

    Update ReactNativeNotificationsHandler.java

--- android/src/main/java/com/azure/reactnative/notificationhub/ReactNativeNotificationsHandler.java
+++ android/src/main/java/com/azure/reactnative/notificationhub/ReactNativeNotificationsHandler.java
@@ -1,30 +1,12 @@
-package com.azure.reactnative.notificationhub;
-
-import android.app.AlarmManager;
-import android.app.Application;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
-
-import com.facebook.react.bridge.ReadableMap;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import com.microsoft.windowsazure.notifications.NotificationsHandler;
+    import android.app.NotificationManager;
+    import android.app.PendingIntent;
+    import android.content.Context;
+    import android.content.Intent;
+    import android.media.RingtoneManager;
+    import android.net.Uri;
+    import android.os.Bundle;
+    import android.support.v4.app.NotificationCompat;
+    import com.microsoft.windowsazure.notifications.NotificationsHandler;
 
 public class ReactNativeNotificationsHandler extends NotificationsHandler {
         private static final String LOG_TAG = "AzureNotificationHub";
